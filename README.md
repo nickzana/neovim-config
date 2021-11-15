191bc48ac8f17548969357db5e223c6f9ddf8bc8
Nick Zana <me@nickzana.dev>
2021-11-15 02:24:04 -0500

    Add texlab LSP support

---
+++ README.md
@@ -0,0 +1,12 @@
+# DEPENDENCIES FOR TEXLAB
+(All packages are as listed in fedora repos)
+* Texlab <https://git.nickzana.dev/mirrors/texlab>
+* Tectonic <https://git.nickzana.dev/mirrors/tectonic>
+	- fontconfig-devel
+	- freetype-devel
+	- graphite2-devel
+	- libicu-devel
+	- openssl-devel
+* zathura
+* texlive-chktex
+
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,33 @@
 local on_attach = require'keybindings'.on_attach
 local capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local cmp = require'nvim-cmp-cfg'.cmp
+
+-- texlab with tectonic
+require'lspconfig'.texlab.setup {
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "texlab" },
+	filetypes = { "tex", "bib" },
+	settings = {
+		texlab = {
+			auxDirectory = ".",
+			bibtexFormatter = "texlab",
+			build = {
+				executable = 'tectonic',
+				args = { "%f", "--synctex", "--keep-logs", "--keep-intermediates" },
+				onSave = true,
+				forwardSearchAfter = true,
+			},
+			forwardSearch = {
+				executable = "zathura",
+				-- Per https://github.com/latex-lsp/texlab/blob/master/docs/previewing.md
+				args = {"--synctex-forward", "%l:1:%f", "%p"},
+			},
+			chktex = {
+				onOpenAndSave = true,
+				onEdit = true,
+			},
+		}
+	},
+	single_file_support = true
+}
