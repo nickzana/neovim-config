d3a2ba714f177ebbb9268caebbcebde0ff66a4fb
Nick Zana <me@nickzana.dev>
2021-11-16 23:01:34 -0500

    Replace vsnips with UltiSnips

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,9 +28,9 @@ local plugins = require('packer').startup(function(use)
 	use 'hrsh7th/cmp-cmdline'
 	use 'hrsh7th/nvim-cmp'		-- Autocompletion
 
-	-- Snippets (Required for nvim-cmp)
-	use 'hrsh7th/cmp-vsnip'
-	use 'hrsh7th/vim-vsnip'
+	-- UltiSnips
+	use 'SirVer/ultisnips'
+	use 'quangnguyen30192/cmp-nvim-ultisnips'
 
 
   if packer_bootstrap then
