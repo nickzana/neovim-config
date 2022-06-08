b83a640109869e3a15c62e85e90e23104ea5a724
Nick Zana <me@nickzana.dev>
2022-06-07 20:42:58 -0400

    Add gopls language server

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -24,6 +24,11 @@ lspconfig.rust_analyzer.setup{
 	}
 }
 
+lspconfig.gopls.setup {
+	on_attach = on_attach,
+	capabilities=capabilities,
+}
+
 -- clangd
 lspconfig.clangd.setup{
 	on_attach = on_attach,
