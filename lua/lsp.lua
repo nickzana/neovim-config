040791ec40fe30d3b8f2194ed68f5cee08f90dce
Nick Zana <me@nickzana.dev>
2022-11-20 18:15:52 -0500

    update nvim-cmp function names

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,7 +1,7 @@
 local on_attach = require'keybindings'.on_attach
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
